e9f2501ad07037ca71a4353564533e9f9e5f4d14
quilo <quilo@CEE-DESKTOP>
2024-01-29 20:43:35 +0800

    Add project files.

---
+++ AdminFinal/Scripts/myScripts/AdminMain.js
@@ -0,0 +1,148 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getDatabase, get, ref } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { getStorage, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
+
+$(document).ready(function () {
+
+   
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBrmIkzH9xI9BHHSJOJMYDd-J3UkPJsS7k",
+        authDomain: "parkbai-c8f04.firebaseapp.com",
+        databaseURL: "https://parkbai-c8f04-default-rtdb.firebaseio.com",
+        projectId: "parkbai-c8f04",
+        storageBucket: "parkbai-c8f04.appspot.com",
+        messagingSenderId: "195961929914",
+        appId: "1:195961929914:web:f609827668b79399b80283",
+        measurementId: "G-0THPRYGBY6"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const auth = getAuth(app);
+
+
+    //PARKBAI ICON FETCHING
+    const imgRef = ref(db, '/ADMIN/ASSETS/Icon');
+    get(imgRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const imgElement = document.getElementById('ParkBaiIcon');
+            imgElement.src = snapshot.val();
+        }
+        else {
+            console.error('File path not found in database.');
+        }
+    }).catch((error) => {
+        console.error('Error fetching file', error);
+    });
+
+    //GENBAL FETCH
+    const balRef = ref(db, 'ADMIN/general_balance');
+    get(balRef).then((snapshot) => {
+        var adminData = snapshot.val().toFixed(2);
+        if (snapshot.exists()) {
+                   
+            var bal = numberWithSpaces(adminData);
+            document.getElementById('genBal').value = "₱ " + bal;
+            
+        }
+        else {
+            console.error('File path not found in database.');
+        }
+    })
+
+    //PARKBAI BALANCE FETCH
+    const balPBRef = ref(db, 'ADMIN/parkbai_balance');
+    get(balPBRef).then((snapshot) => {
+        var adminData = snapshot.val().toFixed(2);
+        if (snapshot.exists()) {
+
+            var bal = numberWithSpaces(adminData);
+            document.getElementById('parkBaiBal').value = "₱ " + bal;
+
+        }
+        else {
+            console.error('File path not found in database.');
+        }
+    })
+
+    //arrange the numbers
+    function numberWithSpaces(x) {
+        var parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        return parts.join(".");
+    }
+
+
+    //get sum of all driver balance
+    const driverRef = ref(db, 'DRIVER');
+    get(driverRef).then((snapshot) => {
+        const DriverUID = [];
+
+        snapshot.forEach((childSnapshot) => {
+            const childId = childSnapshot.key;
+            DriverUID.push(childId);
+        });
+
+        // Use Promise.all to wait for all asynchronous calls to complete
+        Promise.all(DriverUID.map(getDriver)).then((driverBalances) => {
+            const totalDriverBal = driverBalances.reduce((acc, bal) => acc + bal, 0);
+            const totDriverBal = totalDriverBal.toFixed(2);
+            const totBal = numberWithSpaces(totDriverBal);
+            // Display the total balance on the button
+            document.getElementById('balDriver').value = "₱ " + totBal;
+
+
+        });
+    });
+
+    function getDriver(driverUID) {
+        const VehicleRef = ref(db, `DRIVER/${driverUID}/ACCOUNT/balance`);
+
+        return get(VehicleRef).then((snapshot) => {
+            const bal = snapshot.val() || 0;
+            return bal;
+        });
+    };
+
+
+    //get sum of all Owner balance
+    const ownerRef = ref(db, 'PARK_OWNER');
+    get(ownerRef).then((snapshot) => {
+        const OwnerUID = [];
+
+        snapshot.forEach((childSnapshot) => {
+            const childId = childSnapshot.key;
+            OwnerUID.push(childId);
+        });
+
+        // Use Promise.all to wait for all asynchronous calls to complete
+        Promise.all(OwnerUID.map(getOwner)).then((ownerBalances) => {
+            const totalOwnerBal = ownerBalances.reduce((acc, bal) => acc + bal, 0);
+            const totOwnerBal = totalOwnerBal.toFixed(2);
+            const totBal = numberWithSpaces(totOwnerBal);
+            // Display the total balance on the button
+            document.getElementById('balOwner').value = "₱ " + totBal;
+            
+        });
+    });
+
+    function getOwner(ownerUID) {
+        const VehicleRef = ref(db, `PARK_OWNER/${ownerUID}/INCOME/Current_Balance`);
+
+        return get(VehicleRef).then((snapshot) => {
+            const bal = snapshot.val() || 0;
+            return bal;
+        });
+    };
+
+    $("#btnDriver").click(function () {
+        window.location.href = `/Home/DriverBal`;
+    });
+
+    $("#btnOwner").click(function () {
+        window.location.href = `/Home/OwnerBal`;
+    });
+    
+});
---
+++ AdminFinal/Scripts/myScripts/OwnerBal.js
@@ -0,0 +1,127 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getDatabase, ref, child, onValue, get, update } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+$(document).ready(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBrmIkzH9xI9BHHSJOJMYDd-J3UkPJsS7k",
+        authDomain: "parkbai-c8f04.firebaseapp.com",
+        databaseURL: "https://parkbai-c8f04-default-rtdb.firebaseio.com",
+        projectId: "parkbai-c8f04",
+        storageBucket: "parkbai-c8f04.appspot.com",
+        messagingSenderId: "195961929914",
+        appId: "1:195961929914:web:f609827668b79399b80283",
+        measurementId: "G-0THPRYGBY6"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+
+    var uid = 0;
+    var drvNo = 0;
+
+
+
+    const dbRef = ref(db, 'ADMIN/TRANSACTIONS/OWNER');
+
+    //GETTING THE DRIVERS UIDS
+    get(dbRef).then((snapshot) => {
+        const OwnerUID = [];
+
+        snapshot.forEach((childSnapshot) => {
+            // Collect the child IDs (keys)
+            const childId = childSnapshot.key;
+            OwnerUID.push(childId);
+
+        });
+        console.log("Child IDs:", OwnerUID);
+        getOwner(OwnerUID);
+    });
+
+    //GETTING THE VEHICLE PLATE NUMBER
+    function getOwner(OwnerUID) {
+        var itemsPerPage = 8;
+        var currentPage = 1;
+        var table = $("#table");
+        var tbody = $('#tbody');
+
+        function displayTablePage(page, data) {
+            tbody.empty();
+            var startIndex = (page - 1) * itemsPerPage;
+            var endIndex = startIndex + itemsPerPage;
+            var resultRow = 0;
+
+            for (var i = startIndex; i < endIndex && i < data.length; i++) {
+                const details = data[i];
+                let trow = $('<tr>');
+                let td1 = $('<td>').text(details.Name).addClass('fitText');
+                let td2 = $('<td>').text(details.Amount);
+                let td3 = $('<td>').text(details.Date);
+                let td4 = $('<td>').text(details.Ref_Num);
+                trow.append(td1, td2, td3, td4);
+                tbody.append(trow);
+                resultRow++;
+            }
+
+            $('#currentPage').text(page);
+
+            if (currentPage === 1) {
+                $('#prevBtn').prop("disabled", true).addClass("inactive");
+            } else {
+                $('#prevBtn').prop("disabled", false).removeClass("inactive");
+            }
+
+            var totalPages = Math.ceil(data.length / itemsPerPage);
+
+            if (page >= totalPages) {
+                $('#nextBtn').prop("disabled", true).addClass("inactive");
+            } else {
+                $('#nextBtn').prop("disabled", false).removeClass("inactive");
+            }
+        }
+
+        // Fetch data from Firebase
+        var promises = OwnerUID.map(uid => {
+            const OwnRef = ref(db, "ADMIN/TRANSACTIONS/OWNER/" + uid);
+            return get(OwnRef).then(snapshot => snapshot.val());
+        });
+
+        Promise.all(promises).then(data => {
+            // Sort data based on the date field in descending order
+            data.sort((a, b) => {
+                const dateA = parseDate(a.Date);
+                const dateB = parseDate(b.Date);
+                return dateB - dateA;
+            });
+
+            // Initial display of the first page
+            displayTablePage(currentPage, data);
+
+            // Event handlers for pagination buttons
+            $('#prevBtn').on('click', function () {
+                if (currentPage > 1) {
+                    currentPage--;
+                    displayTablePage(currentPage, data);
+                }
+            });
+
+            $('#nextBtn').on('click', function () {
+                var totalPages = Math.ceil(data.length / itemsPerPage);
+                if (currentPage < totalPages) {
+                    currentPage++;
+                    displayTablePage(currentPage, data);
+                }
+            });
+        });
+    }
+
+    function parseDate(dateString) {
+        const [day, month, year] = dateString.split('/');
+        return new Date(`${month}/${day}/${year}`);
+    }
+
+    //add
+  
+   
+
+
+});
